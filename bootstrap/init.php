88557b7e7b2760bb20ce8a77a204c9fb0d067f16
Ammar Faizi <ammarfaizi2@gmail.com>
2018-11-15 22:13:45 +0700

    Initial commit

---
+++ bootstrap/init.php
@@ -0,0 +1,38 @@
+<?php
+
+if (!defined("_ICETEA_INIT")) {
+
+	define("_ICETEA_INIT", 1);
+	
+	/**
+	 * Load init config.
+	 */
+	require __DIR__."/../config/init.php";
+	
+	/**
+	 * @param string $class
+	 * @return void
+	 */
+	function iceteaInternalClassAutoloader(string $class): void
+	{
+		$class = str_replace("\\", "/", $class);
+		if (file_exists($class = BASEPATH."/src/classes/{$class}.php")) {
+			require $class;
+		}
+	}
+
+	/**
+	 * Register autoload.
+	 */
+	spl_autoload_register("iceteaInternalClassAutoloader");
+
+	/**
+	 * Load composer autoload.
+	 */
+	require BASEPATH."/vendor/autoload.php";
+
+	/**
+	 * Load helper functions.
+	 */
+	require BASEPATH."/src/helpers.php";
+}
