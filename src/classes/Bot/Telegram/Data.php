6324da97adb91f0f6ca49f60cd3903bd410a5bc1
Ammar Faizi <ammarfaizi2@gmail.com>
2018-11-15 22:51:47 +0700

    Create InvalidArrayIndexException and update data handler

--- src/classes/Bot/Telegram/Data.php
+++ src/classes/Bot/Telegram/Data.php
@@ -4,6 +4,8 @@ namespace Bot\Telegram;
 
 use ArrayAccess;
 use JsonSerializable;
+use Exceptions\InvalidArrayIndexException;
+use Exceptions\InvalidJsonFormatException;
 
 /**
  * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
@@ -16,7 +18,14 @@ final class Data implements ArrayAccess, JsonSerializable
 	/**
 	 * @var mixed
 	 */
-	private $in;
+	public $in;
+
+	/**
+	 * The container.
+	 *
+	 * @var array
+	 */
+	private $c = [];
 
 	/**
 	 * @param string $json
@@ -38,5 +47,54 @@ final class Data implements ArrayAccess, JsonSerializable
 		if (!is_array($this->in)) {
 			throw new InvalidJsonFormatException("JSON must be a valid array.");
 		}
+	}
+
+	/**
+	 * @throws \Exceptions\InvalidArrayIndexException
+	 * @param mixed $offset
+	 * @return mixed
+	 */
+	public function &offsetGet($offset)
+	{	
+		if (array_key_exists($offset, $this->c)) {
+			return $this->c[$offset];
+		}
+		throw new InvalidArrayIndexException("Invalid offset {$offset}");
+	}
+
+	/**
+	 * @param mixed $offset
+	 * @return bool
+	 */
+	public function offsetExists($offset): bool
+	{
+		return isset($this->c[$offset]);
+	}
+
+	/**
+	 * @param mixed $offset
+	 * @param mixed $value
+	 * @return void
+	 */
+	public function offsetSet($offset, $value): void
+	{
+		$this->c[$offset] = $value;
+	}
+
+	/**
+	 * @param mixed $offset
+	 * @return void
+	 */
+	public function offsetUnset($offset): void
+	{
+		unset($this->c[$offset]);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function jsonSerialize(): array
+	{
+		return is_array($this->c) ? $this->c : [];
 	}
 }
