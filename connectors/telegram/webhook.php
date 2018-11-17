9ab3fe84762c325f7041c6f5efe1c54ad99112fe
Ammar Faizi <ammarfaizi2@gmail.com>
2018-11-17 10:43:15 +0700

    Prepare shared object dir and update webhook

--- connectors/telegram/webhook.php
+++ connectors/telegram/webhook.php
@@ -2,10 +2,12 @@
 
 $input = escapeshellarg(file_get_contents("php://input"));
 
+$dir = __DIR__;
+
 shell_exec(
-	"nohup php ".__DIR__."/webhook_worker.php {$input} >> ".__DIR__."/../logs/telegram/webhook_worker.log 2>&1 &"
+	"nohup php {$dir}/webhook_worker.php {$input} >> {$dir}/../../logs/telegram/webhook_worker.log 2>&1 &"
 );
 
 shell_exec(
-	"nohup php ".__DIR__."/logger.php {$input} >> ".__DIR__."/../logs/telegram/logger.log 2>&1 &"
+	"nohup php {$dir}/logger.php {$input} >> {$dir}/../../logs/telegram/logger.log 2>&1 &"
 );
