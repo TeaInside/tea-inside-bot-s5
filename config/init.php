c79423e1a9dcd407c94a8e7cd17fbd5261e8ed6f
Ammar Faizi <ammarfaizi2@gmail.com>
2018-11-22 16:39:06 +0700

    Update init config

--- config/init.php
+++ config/init.php
@@ -1,6 +1,8 @@
 <?php
 
-if (defined("INIT_CONFIG")) {
+if (!defined("INIT_CONFIG")) {
+	define("INIT_CONFIG", 1);
+	
 	define("BASEPATH", realpath(__DIR__."/.."));
 	define("PUBLIC_PATH", BASEPATH."/public");
 	define("STORAGE_PATH", BASEPATH."/storage");
