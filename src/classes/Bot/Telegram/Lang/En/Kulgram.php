05eae4ae3a25af66285a8a2c053bdc39d065e284
Ammar Faizi <ammarfaizi2@gmail.com>
2018-12-23 15:48:16 +0700

    [lang:en] Create kulgram language data

---
+++ src/classes/Bot/Telegram/Lang/En/Kulgram.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Bot\Telegram\Lang\En;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
+ * @license MIT
+ * @version 5.0.0
+ * @since 2.0.1
+ * @package \Bot\Telegram\Lang\Id
+ */
+class Kulgram
+{
+	public static $l = [
+		"intro" => 
+"Usage: /kulgram [command] [option]
+Commands:
+ help Show this message
+ init Initialize a kulgram session
+ start Start a kulgram session
+ stop  Stop a kulgram session
+ cancel Cancel a kulgram session",
+ 		"unknown" => "Unknown command `:cmd`. Use /kulgram help to show all commands."
+	];
+}
