6d55d04baa81f73840e38ce60db159a0ba5e2eda
Ammar Faizi <ammarfaizi2@gmail.com>
2018-12-23 14:07:17 +0700

    [command] Create Kulgram class (package: \Bot\Telegram\Responses)

---
+++ src/classes/Bot/Telegram/Responses/Kulgram.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Bot\Telegram\Responses;
+
+use Bot\Telegram\Exe;
+use Bot\Telegram\Lang;
+use Bot\Telegram\ResponseFoundation;
+use Bot\Telegram\Utils\GroupSetting;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
+ * @license MIT
+ * @version 5.0.0
+ * @package \Bot\Telegram\Responses
+ */
+class Kulgram extends ResponseFoundation
+{
+	/**
+	 * @var \Bot\Telegram\Data
+	 */
+	protected $d;
+
+	/**
+	 * @param \Bot\Telegram\Data $d
+	 *
+	 * Constructor.
+	 */
+	public function __construct(Data $d)
+	{
+		$this->d = $d;
+
+	}
+
+	/**
+	 * @param string $cmd
+	 * @return bool
+	 */
+	public function run(string $cmd): bool
+	{
+		
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function init(): bool
+	{
+
+		return true;
+	}
+}
