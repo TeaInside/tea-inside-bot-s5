32c7df33129a93af682f50894f08d4ba462fdd13
Ammar Faizi <ammarfaizi2@gmail.com>
2018-12-31 09:45:04 +0700

    [last log] Add debug file on webhook_worker.php

--- connectors/telegram/webhook_worker.php
+++ connectors/telegram/webhook_worker.php
@@ -3,6 +3,8 @@
 require __DIR__."/../../bootstrap/init.php";
 require BASEPATH."/config/telegram/config.php";
 
+file_put_contents(BASEPATH."/logs/last.log", json_encode(json_decode($argv[1]), 128));
+
 if (isset($argv[1])) {
 	$st = new Bot\Telegram\Bot($argv[1]);
 	$st->run();
--- tests/dummy.php
+++ tests/dummy.php
@@ -1,33 +1,48 @@
 <?php
 
 $argv[1] = '{
-    "update_id": 345133577,
-    "message": {
-        "message_id": 123090,
-        "from": {
+    "message_id": 127248,
+    "from": {
+        "id": 243692601,
+        "is_bot": false,
+        "first_name": "Ammar",
+        "last_name": "Faizi",
+        "username": "ammarfaizi2",
+        "language_code": "en"
+    },
+    "chat": {
+        "id": -1001162202776,
+        "title": "Koding Teh",
+        "username": "KodingTeh",
+        "type": "supergroup"
+    },
+    "date": 1546223988,
+    "new_chat_participant": {
+        "id": 243692601,
+        "is_bot": false,
+        "first_name": "Ammar",
+        "last_name": "Faizi",
+        "username": "ammarfaizi2",
+        "language_code": "en"
+    },
+    "new_chat_member": {
+        "id": 243692601,
+        "is_bot": false,
+        "first_name": "Ammar",
+        "last_name": "Faizi",
+        "username": "ammarfaizi2",
+        "language_code": "en"
+    },
+    "new_chat_members": [
+        {
             "id": 243692601,
             "is_bot": false,
             "first_name": "Ammar",
             "last_name": "Faizi",
             "username": "ammarfaizi2",
             "language_code": "en"
-        },
-        "chat": {
-            "id": -1001162202776,
-            "title": "Koding Teh",
-            "username": "KodingTeh",
-            "type": "supergroup"
-        },
-        "date": 1545553737,
-        "text": "<?c\n#include <stdio.h>\nint main(){printf(\"test\");}",
-        "entities": [
-            {
-                "offset": 0,
-                "length": 8,
-                "type": "bot_command"
-            }
-        ]
-    }
+        }
+    ]
 }';
 
 require __DIR__."/../connectors/telegram/webhook_worker.php";
