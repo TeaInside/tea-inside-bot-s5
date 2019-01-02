c71c659a203d5b094c4798e6c337887bcb61a1f8
Ammar Faizi <ammarfaizi2@gmail.com>
2019-01-02 16:41:38 +0700

    [master] Create set welcome cmd

---
+++ src/classes/Bot/Telegram/Lang/En/Welcome.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Bot\Telegram\Lang\En;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
+ * @license MIT
+ * @version 5.0.0
+ * @since 2.0.1
+ * @package \Bot\Telegram\Lang\Id
+ */
+class Welcome
+{
+	public static $l = [
+		"ok" => "Welcome message has been successfully saved!",
+		"error" => "<b>An error occured:</b> Invalid welcome message\n\nTelegram response: :error",
+		"unknown_error" => "Internal error"
+	];
+}
--- src/classes/Bot/Telegram/Responses/Welcome.php
+++ src/classes/Bot/Telegram/Responses/Welcome.php
@@ -5,6 +5,7 @@ namespace Bot\Telegram\Responses;
 use DB;
 use PDO;
 use Bot\Telegram\Exe;
+use Bot\Telegram\Lang;
 use Bot\Telegram\ResponseFoundation;
 
 /**
@@ -21,7 +22,7 @@ class Welcome extends ResponseFoundation
 	 */
 	public function setWelcome(string $welcomeMessage): bool
 	{
-		$out = Exe::sendMessage(
+		$o = Exe::sendMessage(
 			[
 				"chat_id" => $this->d["chat_id"],
 				"reply_to_message_id" => $this->d["msg_id"],
@@ -30,15 +31,54 @@ class Welcome extends ResponseFoundation
 			]
 		);
 
+		$o = json_decode($o, true);
 
-		Exe::sendMessage(
-			[
-				"chat_id" => $this->d["chat_id"],
-				"reply_to_message_id" => $this->d["msg_id"],
-				"text" => $out["out"],
-				"parse_mode" => "HTML"
-			]
-		);
+		if (isset($o["ok"]) && $o["ok"]) {
+
+			DB::pdo()
+				->prepare("UPDATE `group_settings` SET `welcome_message` = :welcome_message WHERE `group_id` = :group_id LIMIT 1")
+				->execute(
+					[
+						":welcome_message" => $welcomeMessage,
+						":group_id" => $this->d["chat_id"]
+					]
+				);
+
+			Exe::sendMessage(
+				[
+					"chat_id" => $this->d["chat_id"],
+					"reply_to_message_id" => $this->d["msg_id"],
+					"text" => Lang::getInstance()->get("Welcome", "ok"),
+					"parse_mode" => "HTML"
+				]
+			);
+
+		} else {
+			if (isset($o["error_code"], $o["description"])) {
+				Exe::sendMessage(
+					[
+						"chat_id" => $this->d["chat_id"],
+						"reply_to_message_id" => $this->d["msg_id"],
+						"text" => Lang::bind(
+							Lang::getInstance()->get("Welcome", "error"),
+							[
+								":error" => "<code>".htmlspecialchars("{$o["error_code"]} {$o["description"]}", ENT_QUOTES, "UTF-8")."</code>"
+							]
+						),
+						"parse_mode" => "HTML"
+					]
+				);
+			}
+
+			Exe::sendMessage(
+					[
+						"chat_id" => $this->d["chat_id"],
+						"reply_to_message_id" => $this->d["msg_id"],
+						"text" => Lang::getInstance()->get("Welcome", "unknown_error"),
+						"parse_mode" => "HTML"
+					]
+				);
+		}
 
 		return true;
 	}
