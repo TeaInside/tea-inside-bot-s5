a1fff694b4353b089abf015e5b344e396a42ee40
Ammar Faizi <ammarfaizi2@gmail.com>
2019-01-02 17:08:31 +0700

    [master] Debug promote cmd

--- src/classes/Bot/Telegram/Responses/Promote.php
+++ src/classes/Bot/Telegram/Responses/Promote.php
@@ -21,8 +21,27 @@ class Ping extends ResponseFoundation
 	public function promote(): bool
 	{
 
-		if (isset($this->d["reply_to_message"])) {
+		if (isset($this->d["reply_to_message"]["from"]["id"])) {
+			$o = Exe::promoteChatMember(
+				[
+					"chat_id" => $this->d["chat_id"],
+					"user_id" => $this->d["reply_to_message"]["from"]["id"],
+					"can_change_info" => 1,
+					"can_delete_messages" => 1,
+					"can_invite_users" => 1,
+					"can_restrict_members" => 1,
+					"can_pin_messages" => 1,
+					"can_promote_members" => 1
+				]
+			);
 
+			Exe::sendMessage(
+				[
+					"chat_id" => $this->d["chat_id"],
+					"text" => $o["out"],
+					"reply_to_message_id" => $this->d["msg_id"]
+				]
+			);
 		}
 
 		return true;
