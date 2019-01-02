29d5f66884048ec77ead73f3826b306fd1db3a1e
Ammar Faizi <ammarfaizi2@gmail.com>
2019-01-02 17:22:56 +0700

    [master] Fix promote lang class name

---
+++ src/classes/Bot/Telegram/Lang/En/Promote.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bot\Telegram\Lang\En;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
+ * @license MIT
+ * @version 5.0.0
+ * @since 2.0.1
+ * @package \Bot\Telegram\Lang\Id
+ */
+class Promote
+{
+	public static $l = [
+		"promote.ok" => ":ulink has been promoted to be an administrator!",
+		"promote.error" => "<b>An error occured!</b>\n\n:error_message"
+	];
+}
